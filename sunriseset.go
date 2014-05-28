fe206574de6df8657972aaeff2ace7a58761c8bd
six-heyaxiang <six.heyaxiang@gmail.com>
2014-05-28 17:30:42 +0800

    v1.0

--- sunriseset.go
+++ sunriseset.go
@@ -11,10 +11,10 @@ const h = -0.833
 const pi = 3.1415126
 
 type SunRiseSet struct {
-	uto  float64 `float64:180.0`
-	lat  float64
-	long float64
-	date string
+	Uto  float64 `float64:180.0`
+	Lat  float64
+	Long float64
+	Date string
 	rise string
 	set  string
 }
@@ -31,13 +31,13 @@ func (s *SunRiseSet) GetSet() string {
 
 //SunRise 计算日出时间
 func (s *SunRiseSet) SunRise() *SunRiseSet {
-	s.rise = toTime(result_rise(ut_rise(s.uto, gha(s.uto, g_sun(t_century(days(s.date), s.uto)), ecliptic_longitude(l_sun(t_century(days(s.date), s.uto)), g_sun(t_century(days(s.date), s.uto)))), s.long, e(h, s.lat, sun_deviation(earth_tilt(t_century(days(s.date), s.uto)), ecliptic_longitude(l_sun(t_century(days(s.date), s.uto)), g_sun(t_century(days(s.date), s.uto)))))), s.uto, s.long, s.lat, s.date))
+	s.rise = toTime(result_rise(ut_rise(s.Uto, gha(s.Uto, g_sun(t_century(days(s.Date), s.Uto)), ecliptic_Longitude(l_sun(t_century(days(s.Date), s.Uto)), g_sun(t_century(days(s.Date), s.Uto)))), s.Long, e(h, s.Lat, sun_deviation(earth_tilt(t_century(days(s.Date), s.Uto)), ecliptic_Longitude(l_sun(t_century(days(s.Date), s.Uto)), g_sun(t_century(days(s.Date), s.Uto)))))), s.Uto, s.Long, s.Lat, s.Date))
 	return s
 }
 
 //SunSet 计算日落时间
 func (s *SunRiseSet) SunSet() *SunRiseSet {
-	s.set = toTime(result_set(ut_rise(s.uto, gha(s.uto, g_sun(t_century(days(s.date), s.uto)), ecliptic_longitude(l_sun(t_century(days(s.date), s.uto)), g_sun(t_century(days(s.date), s.uto)))), s.long, e(h, s.lat, sun_deviation(earth_tilt(t_century(days(s.date), s.uto)), ecliptic_longitude(l_sun(t_century(days(s.date), s.uto)), g_sun(t_century(days(s.date), s.uto)))))), s.uto, s.long, s.lat, s.date))
+	s.set = toTime(result_set(ut_rise(s.Uto, gha(s.Uto, g_sun(t_century(days(s.Date), s.Uto)), ecliptic_Longitude(l_sun(t_century(days(s.Date), s.Uto)), g_sun(t_century(days(s.Date), s.Uto)))), s.Long, e(h, s.Lat, sun_deviation(earth_tilt(t_century(days(s.Date), s.Uto)), ecliptic_Longitude(l_sun(t_century(days(s.Date), s.Uto)), g_sun(t_century(days(s.Date), s.Uto)))))), s.Uto, s.Long, s.Lat, s.Date))
 	return s
 }
 
@@ -47,8 +47,8 @@ func toTime(temp float64) string {
 }
 
 //days 求从格林威治时间公元2000年1月1日到计算日天数days
-func days(date string) int {
-	end, err := time.Parse("2006-01-02", date)
+func days(Date string) int {
+	end, err := time.Parse("2006-01-02", Date)
 	if err != nil {
 		log.Println("Fatal error:%s", err.Error())
 	}
@@ -80,38 +80,38 @@ func earth_tilt(t_century float64) float64 {
 	return (23.4393 - 0.0130*t_century)
 }
 
-//ecliptic_longitude 求黄道经度
-func ecliptic_longitude(l_sun, g_sun float64) float64 {
+//ecliptic_Longitude 求黄道经度
+func ecliptic_Longitude(l_sun, g_sun float64) float64 {
 	return (l_sun + 1.915*math.Sin(g_sun*pi/180) + 0.02*math.Sin(2*g_sun*pi/180))
 }
 
 //gha 求格林威治时间的太阳时间角GHA
-func gha(UTo, g_sun, ecliptic_longitude float64) float64 {
-	return (UTo - 180 - 1.915*math.Sin(g_sun*pi/180) - 0.02*math.Sin(2*g_sun*pi/180) + 2.466*math.Sin(2*ecliptic_longitude*pi/180) - 0.053*math.Sin(4*ecliptic_longitude*pi/180))
+func gha(UTo, g_sun, ecliptic_Longitude float64) float64 {
+	return (UTo - 180 - 1.915*math.Sin(g_sun*pi/180) - 0.02*math.Sin(2*g_sun*pi/180) + 2.466*math.Sin(2*ecliptic_Longitude*pi/180) - 0.053*math.Sin(4*ecliptic_Longitude*pi/180))
 }
 
 //sun_deviation
-func sun_deviation(earth_tilt, ecliptic_longitude float64) float64 {
-	return (180 / pi * math.Asin(math.Sin(pi/180*earth_tilt)*math.Sin(pi/180*ecliptic_longitude)))
+func sun_deviation(earth_tilt, ecliptic_Longitude float64) float64 {
+	return (180 / pi * math.Asin(math.Sin(pi/180*earth_tilt)*math.Sin(pi/180*ecliptic_Longitude)))
 }
 
 //e 求修正值e
-func e(h, glat, sun_deviation float64) float64 {
-	return 180 / pi * math.Acos((math.Sin(h*pi/180)-math.Sin(glat*pi/180)*math.Sin(sun_deviation*pi/180))/(math.Cos(glat*pi/180)*math.Cos(sun_deviation*pi/180)))
+func e(h, gLat, sun_deviation float64) float64 {
+	return 180 / pi * math.Acos((math.Sin(h*pi/180)-math.Sin(gLat*pi/180)*math.Sin(sun_deviation*pi/180))/(math.Cos(gLat*pi/180)*math.Cos(sun_deviation*pi/180)))
 }
 
 //ut_rise 求日出时间
-func ut_rise(UTo, GHA, glong, e float64) float64 {
-	return (UTo - (GHA + glong + e))
+func ut_rise(UTo, GHA, gLong, e float64) float64 {
+	return (UTo - (GHA + gLong + e))
 }
 
 //ut_set 求日落时间
-func ut_set(UTo, GHA, glong, e float64) float64 {
-	return (UTo - (GHA + glong - e))
+func ut_set(UTo, GHA, gLong, e float64) float64 {
+	return (UTo - (GHA + gLong - e))
 }
 
 //result_rise 判断并返回结果（日出）
-func result_rise(UT, UTo, glong, glat float64, date string) float64 {
+func result_rise(UT, UTo, gLong, gLat float64, Date string) float64 {
 	var d float64
 	if UT >= UTo {
 		d = UT - UTo
@@ -121,20 +121,20 @@ func result_rise(UT, UTo, glong, glat float64, date string) float64 {
 	if d >= 0.1 {
 		UTo = UT
 		UT = ut_rise(UTo,
-			gha(UTo, g_sun(t_century(days(date), UTo)),
-				ecliptic_longitude(l_sun(t_century(days(date), UTo)),
-					g_sun(t_century(days(date), UTo)))),
-			glong,
-			e(h, glat, sun_deviation(earth_tilt(t_century(days(date), UTo)),
-				ecliptic_longitude(l_sun(t_century(days(date), UTo)),
-					g_sun(t_century(days(date), UTo))))))
-		result_rise(UT, UTo, glong, glat, date)
+			gha(UTo, g_sun(t_century(days(Date), UTo)),
+				ecliptic_Longitude(l_sun(t_century(days(Date), UTo)),
+					g_sun(t_century(days(Date), UTo)))),
+			gLong,
+			e(h, gLat, sun_deviation(earth_tilt(t_century(days(Date), UTo)),
+				ecliptic_Longitude(l_sun(t_century(days(Date), UTo)),
+					g_sun(t_century(days(Date), UTo))))))
+		result_rise(UT, UTo, gLong, gLat, Date)
 	}
 	return UT
 }
 
 //result_set 判断并返回结果（日落）
-func result_set(UT, UTo, glong, glat float64, date string) float64 {
+func result_set(UT, UTo, gLong, gLat float64, Date string) float64 {
 	var d float64
 	if UT >= UTo {
 		d = UT - UTo
@@ -144,24 +144,24 @@ func result_set(UT, UTo, glong, glat float64, date string) float64 {
 	if d >= 0.1 {
 		UTo = UT
 		UT = ut_set(UTo,
-			gha(UTo, g_sun(t_century(days(date), UTo)),
-				ecliptic_longitude(l_sun(t_century(days(date), UTo)),
-					g_sun(t_century(days(date), UTo)))),
-			glong,
-			e(h, glat, sun_deviation(earth_tilt(t_century(days(date), UTo)),
-				ecliptic_longitude(l_sun(t_century(days(date), UTo)),
-					g_sun(t_century(days(date),
+			gha(UTo, g_sun(t_century(days(Date), UTo)),
+				ecliptic_Longitude(l_sun(t_century(days(Date), UTo)),
+					g_sun(t_century(days(Date), UTo)))),
+			gLong,
+			e(h, gLat, sun_deviation(earth_tilt(t_century(days(Date), UTo)),
+				ecliptic_Longitude(l_sun(t_century(days(Date), UTo)),
+					g_sun(t_century(days(Date),
 						UTo))))))
-		result_set(UT, UTo, glong, glat, date)
+		result_set(UT, UTo, gLong, gLat, Date)
 	}
 	return UT
 }
 
 //zone 时区
-func zone(glong float64) int {
-	if glong >= 0 {
-		return int(glong/15.0) + 1
+func zone(gLong float64) int {
+	if gLong >= 0 {
+		return int(gLong/15.0) + 1
 	} else {
-		return int(glong/15.0) - 1
+		return int(gLong/15.0) - 1
 	}
 }
--- sunriseset_test.go
+++ sunriseset_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestSunriseset(t *testing.T) {
-	srs := &SunRiseSet{uto: 180.0, lat: 34.1234, long: 123.43434, date: "2014-05-28"}
+	srs := &SunRiseSet{Uto: 180.0, Lat: 34.1234, Long: 123.43434, Date: "2014-05-28"}
 	log.Println(srs.SunRise().GetRise())
 	log.Println(srs.SunSet().GetSet())
 }
